d5d9c97605755480a1e1ce544d38fcabbbe3c8b3
João Pedro Silva <jpslima@timbrasil.com.br>
2024-08-26 10:10:43 -0300

    Removed (yet) unused files. Added new keymaps. Added new Comment plugin for better and easier commenting. Changed something on telescope.

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,18 @@
+local k = vim.keymap
+
+local opts = {noremap = true, silent = true}
+
+-- Windows navigation
+k.set("n", "<C-h>", "<C-w>h", opts)
+k.set("n", "<C-j>", "<C-w>j", opts)
+k.set("n", "<C-k>", "<C-w>k", opts)
+k.set("n", "<C-l>", "<C-w>l", opts)
+
+-- Windows control
+k.set("n", "<leader>wv", "<C-w>v", opts) -- Split window vertically
+k.set("n", "<leader>wh", "<C-w>s", opts) -- Split window horizontally
+k.set("n", "<leader>wq", "<C-w>q", opts) -- Close current window
+
+-- Identing
+k.set("v", "<", "<gv", { noremap=false })
+k.set("v", ">", ">gv", { noremap=false })
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,11 +11,20 @@ local config = function ()
                 },
             },
        },
-   })
+  })
 end
+
 return {
     'nvim-telescope/telescope.nvim', branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim' },
     lazy = false,
     config = config,
+    keys = {
+      keymap.set("n", "<leader>ff", ":Telescope find_files<CR>"),  
+      keymap.set("n", "<leader>fk", ":Telescope keymaps<CR>"),  
+      keymap.set("n", "<leader><leader>", ":Telescope <CR>"),  
+      keymap.set("n", "<leader>fb", ":Telescope buffers<CR>"),  
+      keymap.set("n", "<leader>fh", ":Telescope help_tags<CR>"),  
+      keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>"),  
+    }
 }
