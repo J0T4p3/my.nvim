519375dbb0823cb4f5977c5e527a23ad4b78b421
João Pedro Silva <jpslima@timbrasil.com.br>
2024-08-29 09:26:08 -0300

    Commit

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,6 @@
 local k = vim.keymap
 
-local opts = {noremap = true, silent = true}
+local opts = { noremap = true, silent = true }
 
 -- Windows navigation
 k.set("n", "<C-h>", "<C-w>h", opts)
@@ -11,8 +11,9 @@ k.set("n", "<C-l>", "<C-w>l", opts)
 -- Windows control
 k.set("n", "<leader>wv", "<C-w>v", opts) -- Split window vertically
 k.set("n", "<leader>wh", "<C-w>s", opts) -- Split window horizontally
-k.set("n", "<leader>wq", "<C-w>q", opts) -- Close current window
+k.set("n", "<leader>w", "<C-w>q", opts) -- Close current window
 
 -- Identing
-k.set("v", "<", "<gv", { noremap=false })
-k.set("v", ">", ">gv", { noremap=false })
+k.set("v", "<", "<gv", { noremap = false })
+k.set("v", ">", ">gv", { noremap = false })
+
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -13,10 +13,30 @@ local config = function()
 		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 	end
 
+	-- Enable keybindings if available
+	local on_attach = function(bufnr, client)
+		local opts = { noremap = true, silent = true, buffer = bufnr }
+
+		vim.keymap.set("n", "<leader>fd", "<cmd>Lspsaga finder<CR>", opts) -- go to definitionkeymap.set
+		vim.keymap.set("n", "<leader>gd", "<cmd>Lspsaga peek_definition<CR>", opts) -- peak definition
+		vim.keymap.set("n", "<leader>gD", "<cmd>Lspsaga goto_type_definition<CR>", opts) -- go to definition
+		vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_type_action<CR>", opts) -- see available code actions
+		vim.keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
+		vim.keymap.set("n", "<leader>D", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- show  diagnostics for line
+		vim.keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts) -- show diagnostics for cursor
+		vim.keymap.set("n", "<leader>pd", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts) -- jump to prev diagnostic in buffer
+		vim.keymap.set("n", "<leader>nd", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- jump to next diagnostic in buffer
+		vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
+
+		if client.name == "pyright" then
+			vim.keymap.set("n", "<Leader>oi", "<cmd>PyrightOrganizeImports<CR>", opts)
+		end
+	end
+
 	-- lua
 	lsp.lua_ls.setup({
 		-- capabilities = capabilities,
-		-- on_attach = on_attach,
+		on_attach = on_attach,
 		settings = { -- custom settings for lua
 			Lua = {
 				-- make the language server recognize "vim" global
