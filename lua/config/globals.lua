ede949efcf749ead2a40000012adbca13970f814
João Pedro Silva <jpslima@timbrasil.com.br>
2024-09-18 16:55:50 -0300

    Added undotree plugin with custom config and keymaps

--- lua/config/globals.lua
+++ lua/config/globals.lua
@@ -1,2 +1,9 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+-- Global plugins keymappings
+-- Undotree
+vim.g.undotree_SetFocusWhenToggle = 1
+vim.g.undotree_WindowLayout = 4
+vim.g.undotree_ShortIndicators = 1
+vim.g.undotree_SplitWidth = 32
--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,4 +1,8 @@
 return {
 	"mbbill/undotree",
 	lazy = false,
+	keys = {
+		{ "<leader>u", "<cmd>UndotreeToggle<CR>", mode = { "n" }, desc = "Toggles Undothree" },
+	},
+	config = function() end,
 }
