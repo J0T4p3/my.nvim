6b36baefaadcf7f98f2620a5c037e28cacefd69f
João Pedro Silva <jpslima@timbrasil.com.br>
2024-09-19 12:38:25 -0300

    Added New keymaps to facilitate the work; Added LSP functionality; Removed old configuration

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -14,8 +14,26 @@ k.set("n", "ws", "<C-w>s", opts) -- Split window horizontally
 k.set("n", "wq", "<C-w>q", opts) -- Close current window
 
 -- Identing multiple lines
-k.set("v", "<", "<gv", { noremap = false })
-k.set("v", ">", ">gv", { noremap = false })
+k.set("v", "H", "<gv", { noremap = false })
+k.set("v", "L", ">gv", { noremap = false })
+k.set("v", "J", ":m '>+1<CR>gv=gv",{ noremap = false })
+k.set("v", "K", ":m '<-2<CR>gv=gv",{ noremap = false })
 
 -- Open file explorer
 k.set("n", "<leader>ee", "<cmd>Ex<CR>", opts)
+
+-- Buffer navigation and Text mobility
+k.set("n", "J", "mzJ`z") -- Upper the line bellow without changing cursor position
+k.set("n", "<C-d>", "<C-d>zz") -- Do a half scroll without changing cursor position
+k.set("n", "<C-u>", "<C-u>zz")
+k.set("n", "n", "nzzzv") -- Walk trougth searched text 
+k.set("n", "N", "Nzzzv")
+k.set("x", "<leader>p", [["_dP]]) -- Paste yanked text into highlited text without touching the register
+k.set({"n", "v"}, "<leader>d", [["_d]]) -- Delete to void
+k.set("i", "<C-c>", "<Esc>")
+
+-- System's register integration
+k.set({"n", "v"}, "<leader>y", [["+y]])
+k.set("n", "<leader>Y", [["+Y]])
+
+
