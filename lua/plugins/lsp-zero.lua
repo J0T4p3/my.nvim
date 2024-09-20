8ad98165a4ef871f147ccea236c05c8927c5a5b0
João Pedro Silva <jpslima@timbrasil.com.br>
2024-09-20 14:38:29 -0300

    Configured LSP for go, lua and python

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -1,5 +1,17 @@
 return {
-    'VonHeikemen/lsp-zero.nvim', 
+    'VonHeikemen/lsp-zero.nvim',
     branch = 'v4.x',
-    config = false,
+    config = function ()
+        require('lsp-zero').setup({
+            ui = {
+                float_border = 'rounded',
+                sign_text = {
+                    error = '✗',
+                    warn = '▲',
+                    hint = '➜',
+                    info = '»',
+                },
+            }
+        })
+    end
 }
